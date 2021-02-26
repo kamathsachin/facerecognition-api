fdfd4fbb1c998e1ab16c9f3f34548bb8f0826517
Sachin Kamath <kamathsachin@hotmail.com>
2021-02-26 14:11:01 -0600

    Setting up our Server

---
+++ server.js
@@ -0,0 +1,96 @@
+import express from "express";
+import bodyParser from "body-parser";
+import bcrypt from "bcrypt-nodejs";
+
+const app = express();
+app.use(bodyParser.json());
+
+const database = {
+  users: [
+    {
+      id: "123",
+      name: "Sachin",
+      email: "sachin@gmail.com",
+      password: "sachin",
+      entries: 0,
+      joined: new Date(),
+    },
+    {
+      id: "124",
+      name: "Shubha",
+      email: "shubha@gmail.com",
+      password: "shubha",
+      entries: 0,
+      joined: new Date(),
+    },
+  ],
+};
+
+app.get("/", (req, res) => {
+  res.send(database.users);
+});
+
+app.post("/signin", (req, res) => {
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json("Login Successful");
+  } else {
+    res.status(404).json("Invalid Login");
+  }
+});
+
+app.post("/register", (req, res) => {
+  const { name, email, password } = req.body;
+
+  database.users.push({
+    id: "125",
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date(),
+  });
+
+  res.json(database.users[database.users.length - 1]);
+});
+
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  let found = false;
+
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  });
+
+  if (!found) {
+    res.status(404).json("User not found");
+  }
+});
+
+app.put("/image", (req, res) => {
+  const { id } = req.body;
+  let found = false;
+
+  console.log(id);
+
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      user.entries++;
+      return res.json(user.entries);
+    }
+  });
+
+  if (!found) {
+    res.status(404).json("User not found");
+  }
+});
+
+app.listen(3000, () => {
+  console.log("app is running on port 3000");
+});
